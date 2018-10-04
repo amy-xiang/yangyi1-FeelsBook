09316724eec7db9c1bcac7a9fedd66ec1889b717
Amy Xiang <33043340+amy-xiang@users.noreply.github.com>
2018-10-04 13:03:27 -0600

    Polished Emotion Delete

--- app/src/main/java/ca/ualberta/yangyi1_feelsbook/HistoryActivity.java
+++ app/src/main/java/ca/ualberta/yangyi1_feelsbook/HistoryActivity.java
@@ -1,5 +1,8 @@
 package ca.ualberta.yangyi1_feelsbook;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
@@ -38,9 +41,38 @@ class HistoryActivity extends AppCompatActivity {
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
-                Toast.makeText(HistoryActivity.this, "Delete" + emotionArrayList.get(position).toString(), Toast.LENGTH_SHORT).show();
-                Emotion emotion = emotionArrayList.get(position);
-                EmotionListController.getEmotionList().deleteEmotion(emotion);
+                AlertDialog.Builder adb = new AlertDialog.Builder(HistoryActivity.this);
+                String alertMessage = emotionArrayList.get(position).toString() + "\n'" + emotionArrayList.get(position).getComment() + "'";
+                adb.setMessage(alertMessage);
+                adb.setCancelable(true);
+                final int FinalPosition = position;
+
+                adb.setPositiveButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+
+                adb.setNegativeButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Emotion emotion = emotionArrayList.get(FinalPosition);
+                        EmotionListController.getEmotionList().deleteEmotion(emotion);
+                    }
+                });
+
+                adb.setNeutralButton("Edit", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+//                        Intent intent = new Intent(HistoryActivity.this, EditEmotionActivity.class);
+//                        startActivity(intent);
+
+                    }
+                });
+
+
+                adb.show();
+
                 return false;
             }
         });
